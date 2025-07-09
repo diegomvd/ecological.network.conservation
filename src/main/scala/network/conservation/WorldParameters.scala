8a6d50d44c2d589804adae948659a1991aa96a12
Diego <diego.bengocheapaz@pm.me>
2025-07-09 14:22:42 +0200

    Preliminary explorations

--- src/main/scala/network/conservation/WorldParameters.scala
+++ src/main/scala/network/conservation/WorldParameters.scala
@@ -16,6 +16,6 @@ case class WorldParameters(
   def extinctionProbability(status: ProtectionStatus): Double =
     status match
       case Protected => 0.0
-      case Unprotected => 0.75
+      case Unprotected => 0.9
       case null => 0.0
 
